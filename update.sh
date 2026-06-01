abe53f6b4f2e9f59c124fd9e1ee7f1f77784cb95
MANU <manu@MANUs-MacBook-Pro.local>
2026-06-01 23:21:46 +0530

    sileo

--- update.sh
+++ update.sh
@@ -1,3 +1,6 @@
-rm Packages.bz2
-dpkg-scanpackages -m ./pkgs /dev/null >Packages
-bzip2 Packages
+#!/bin/bash
+# Regenerate repo index files for both Cydia and Sileo support
+dpkg-scanpackages -m pkgs /dev/null > Packages
+bzip2 -kf Packages
+xz -kf Packages
+gzip -kf Packages
